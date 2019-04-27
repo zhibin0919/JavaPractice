4d0cd7c07d04a787f0282125e51f6f32a0fcc50e
zhibin <zhibin74@gmail.com>
2019-04-27 21:18:56 +0800

    add TestCollection example

---
+++ src/TestCollection.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String, String> idMapName = new HashMap<>();
+		idMapName.put("1", "Ben");
+		idMapName.put("2", "Sjkok");
+		idMapName.put("3", "John");
+		System.out.println(idMapName);
+	}
+
+}
